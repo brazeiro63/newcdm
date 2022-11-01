46d5eb13c1c5bd6ec27fea35a672a2866ae12c43
brazeiro <brazeiro63@gmail.com>
2022-10-31 22:18:22 -0300

    Implementing carousel

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
-import { Carousel, Item } from 'react-elastic-carousel';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Carousel } from 'react-bootstrap';
 
 const Home = () => {
   return (
     <>
       <h1>Pagina de Home</h1>
       <p>Conteudo da página</p>
-      <Carousel itemsToShow={1}>
-        <Item>1</Item>
-        <Item>2</Item>
-        <Item>3</Item>
-        <Item>4</Item>
-        <Item>5</Item>
-        <Item>6</Item>
-      </Carousel>
+      <div style={{ display: 'block', width: 700, padding: 30 }}>
+        <h4>React-Bootstrap Carousel Component</h4>
+        <Carousel fade>
+          <Carousel.Item interval={3000}>
+            <img
+              className="d-block w-100"
+              src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
+              alt="Image One"
+            />
+            <Carousel.Caption>
+              <h3>Label for first slide</h3>
+              <p>Sample Text for Image One</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={3000}>
+            <img
+              className="d-block w-100"
+              src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
+              alt="Image Two"
+            />
+            <Carousel.Caption>
+              <h3>Label for second slide</h3>
+              <p>Sample Text for Image Two</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
     </>
   );
 };
