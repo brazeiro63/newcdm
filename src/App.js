1ff80278b6a4f62932c7fe262c43d4781c9c0721
brazeiro <brazeiro63@gmail.com>
2022-11-02 13:23:05 -0300

    Footer setup and config

--- src/App.js
+++ src/App.js
@@ -7,19 +7,24 @@ import Comodidades from './Pages/Comodidades.jsx';
 import Anfitrioes from './Pages/Anfitrioes.jsx';
 import Contato from './Pages/Contato.jsx';
 import Imoveis from './Pages/Imoveis.jsx';
+import Footer from './Footer.jsx';
+import Container from './layout/Container.jsx';
 
 export default function App() {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/imoveis" element={<Imoveis />} />
-        <Route path="/atracoes" element={<Atracoes />} />
-        <Route path="/comodidades" element={<Comodidades />} />
-        <Route path="/anfitrioes" element={<Anfitrioes />} />
-        <Route path="/contato" element={<Contato />} />
-      </Routes>
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/imoveis" element={<Imoveis />} />
+          <Route path="/atracoes" element={<Atracoes />} />
+          <Route path="/comodidades" element={<Comodidades />} />
+          <Route path="/anfitrioes" element={<Anfitrioes />} />
+          <Route path="/contato" element={<Contato />} />
+        </Routes>
+      </Container>
+      <Footer />
     </Router>
   );
 }
