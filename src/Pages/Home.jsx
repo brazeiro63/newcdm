fb80337577d21c1821c88ebd83fed8561627eb7d
brazeiro63 <brazeiro63@gmail.com>
2022-11-04 18:01:06 -0300

    Introdução de dados no json

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
 import { Carousel } from 'react-bootstrap';
+import '../style.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const Home = () => {
   return (
