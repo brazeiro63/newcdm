1382e3a9eac1618d1972d43c148e62c35e470db4
brazeiro63 <brazeiro63@gmail.com>
2022-11-05 12:46:50 -0300

    Dinamic carousel solution created,  color settings

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,38 +1,46 @@
 import React from 'react';
-import { Carousel } from 'react-bootstrap';
+import Carousel from 'react-bootstrap/Carousel';
 import '../style.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Image01 from '../assets/images/morrosaopaulo-1.jpeg';
+import Image02 from '../assets/images/morrosaopaulo-2.jpeg';
+import Image03 from '../assets/images/morrosaopaulo-3.jpeg';
+import Image04 from '../assets/images/morrosaopaulo-4.jpeg';
+import Image05 from '../assets/images/morrosaopaulo-5.jpeg';
+import Image06 from '../assets/images/morrosaopaulo-6.jpeg';
+import Image07 from '../assets/images/morrosaopaulo-7.jpeg';
+import Image08 from '../assets/images/morrosaopaulo-8.jpeg';
+import Image09 from '../assets/images/morrosaopaulo-9.jpeg';
 
 const Home = () => {
+  const meusItens = [
+    Image01,
+    Image02,
+    Image03,
+    Image04,
+    Image05,
+    Image06,
+    Image07,
+    Image08,
+    Image09,
+  ];
+
   return (
     <>
       <h1>Pagina de Home</h1>
-      <p>Conteudo da página</p>
-      <div style={{ display: 'block', width: 700, padding: 30 }}>
+      <div className="pages">
         <h4>React-Bootstrap Carousel Component</h4>
         <Carousel fade>
-          <Carousel.Item interval={3000}>
-            <img
-              className="d-block w-100"
-              src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
-              alt="Image One"
-            />
-            <Carousel.Caption>
-              <h3>Label for first slide</h3>
-              <p>Sample Text for Image One</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item interval={3000}>
-            <img
-              className="d-block w-100"
-              src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
-              alt="Image Two"
-            />
-            <Carousel.Caption>
-              <h3>Label for second slide</h3>
-              <p>Sample Text for Image Two</p>
-            </Carousel.Caption>
-          </Carousel.Item>
+          {meusItens.map((item, index) => {
+            return (
+              <Carousel.Item interval={2000} key={index}>
+                <img className="d-block w-100" src={item} alt="Slide" />
+                <Carousel.Caption>
+                  <h3>Caption</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+            );
+          })}
         </Carousel>
       </div>
     </>
